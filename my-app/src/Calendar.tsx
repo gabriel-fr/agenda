42c553ca53b69a2e944d50bf11fb5daee9b18429
Gabriel Ribeiro <gabriel@enzo.com.br>
2022-06-17 22:35:50 -0300

    insercao do calendario

---
+++ my-app/src/Calendar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+
+export default class DemoApp extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locale="pt-Br"
+        themeSystem="Lux"
+      />
+    );
+  }
+}
